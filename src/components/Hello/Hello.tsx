9c8e7a22ecb40ce5f073194bacefeca89e7e3df4
Otis <otis.nguyen.goldenowl@gmail.com>
2021-08-15 19:55:26 +0700

    add template  setting

--- src/components/Hello/Hello.tsx
+++ src/components/Hello/Hello.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const Hello: React.FC = () => <div>hello</div>;
+import classes from './Hello.module.scss';
 
+interface HelloProps {
+  template?: string;
+}
+
+const Hello: React.FC<HelloProps> = ({ template }) => {
+  const temp = 0;
+  return <div>Hello</div>;
+};
+
+Hello.defaultProps = {};
 export default Hello;
